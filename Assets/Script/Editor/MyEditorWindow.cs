15ed9029379badecb525f44af0adad199fe6932e
Penguin-God <gkswh4860@naver.com>
2021-12-24 18:24:42 +0900

    first commit

---
+++ Assets/Script/Editor/MyEditorWindow.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor; // UnityEditor 관련 API 사용하기 위한 지시문
+
+public class MyEditorWindow : MonoBehaviour
+{
+
+}
