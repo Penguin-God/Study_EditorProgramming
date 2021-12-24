73d6208371ccc675f7c270d715ea12dc15e96c5f
Penguin-God <gkswh4860@naver.com>
2021-12-24 18:33:08 +0900

    Hello My First Editor Window

--- Assets/Script/Editor/MyEditorWindow.cs
+++ Assets/Script/Editor/MyEditorWindow.cs
@@ -3,7 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor; // UnityEditor 관련 API 사용하기 위한 지시문
 
-public class MyEditorWindow : MonoBehaviour
+public class MyEditorWindow : EditorWindow // 상속
 {
-
+    // Edit, Window가 있는 메뉴 창에 함수를 실행시킬 경로 추가
+    [MenuItem("MyTool/OpenMyWindow %g")]
+    static void OpenWindow()
+    {
+        MyEditorWindow myWindow = GetWindow<MyEditorWindow>();
+        myWindow.title = "Hello Window";
+    }
 }
